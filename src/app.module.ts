d6386a802000f29ddc77dccd817400da43caa74f
Edison Padilla <edisojp@gmail.com>
2022-05-03 23:03:27 -0400

    feat: implement websocket

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,17 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { ScheduleModule } from '@nestjs/schedule';
+import { SubscribersModule } from './subscribers';
 import { DBModule } from './config/db/db.module';
 
 @Module({
-  imports: [ScheduleModule.forRoot(), DBModule, ...jobsModules, ...Modules],
+  imports: [
+    ScheduleModule.forRoot(),
+    DBModule,
+    ...Modules,
+    ...jobsModules,
+    ...SubscribersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
