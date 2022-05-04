ce17449fa350904c9da5ab35595d34bceaebe94c
Edison Padilla <edisojp@gmail.com>
2022-05-03 23:09:52 -0400

    feat: Redis adapter for webSockets caching

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { RedisIoAdapter } from './resources/adapters/redis-io.adapter';
 
 import swagger from './resources/lib/swagger';
 
@@ -10,6 +11,11 @@ import swagger from './resources/lib/swagger';
   app.enableCors();
 
   swagger(app);
+
+  // Redis adapter for webSockets caching
+  const redisIoAdapter = new RedisIoAdapter(app);
+  await redisIoAdapter.connectToRedis();
+  app.useWebSocketAdapter(redisIoAdapter);
 
   await app.listen(5000);
 })();
